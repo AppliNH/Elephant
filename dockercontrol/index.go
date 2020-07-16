7d4534bad825a744450469234d6993d6a5411936
Thomas Martin <thomas.martindev@gmail.com>
2020-07-16 10:52:23 +0200

    cleaning 🧹

--- dockercontrol/index.go
+++ dockercontrol/index.go
@@ -2,87 +2,17 @@ package dockercontrol
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"strings"
-
-	"time"
 
 	u "applinh/elephant/utils"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
-	"github.com/docker/go-connections/nat"
 )
 
 type Vol struct {
 	Labels map[string]string
 }
 
-func CreateNewContainer(path string) error {
-
-	var dir string
-
-	dir, _ = os.Getwd()
-	path = strings.Replace(path, "/docker-compose.yml", "", -1)
-
-	cli, err := client.NewEnvClient()
-
-	cli.ImagePull(context.Background(), "docker/compose", types.ImagePullOptions{})
-
-	if err != nil {
-		fmt.Println("Unable to create docker client")
-		panic(err)
-	}
-
-	hostBinding := nat.PortBinding{
-		HostIP:   "0.0.0.0",
-		HostPort: "8000",
-	}
-
-	containerPort, err := nat.NewPort("tcp", "80")
-	if err != nil {
-		panic("Unable to get the port")
-	}
-
-	portBinding := nat.PortMap{containerPort: []nat.PortBinding{hostBinding}}
-	cont, err := cli.ContainerCreate(
-		context.Background(),
-		&container.Config{
-			Entrypoint: []string{"/usr/scripts/start.sh"},
-			Image:      "docker/compose",
-		},
-		&container.HostConfig{
-			PortBindings: portBinding,
-			Binds:        []string{dir + "/scripts:/usr/scripts", "/var/run/docker.sock:/var/run/docker.sock", path + ":/var/tmp/docker/compose"},
-		}, nil, "")
-
-	if err != nil {
-		return err
-	}
-
-	cli.ContainerStart(context.Background(), cont.ID, types.ContainerStartOptions{})
-	fmt.Printf("Container %s has started \n", cont.ID)
-	time.Sleep(5 * time.Second)
-
-	r, e := cli.ContainerExecCreate(context.Background(), cont.ID, types.ExecConfig{
-		Cmd:          []string{"/usr/scripts/getinfos.sh"},
-		Tty:          true,
-		AttachStderr: true,
-		AttachStdout: true,
-		AttachStdin:  true,
-		Detach:       true,
-	})
-
-	if e != nil {
-		return e
-	}
-
-	fmt.Println(r)
-
-	return nil
-}
 
 func ListContainers() ([]types.Container, error) {
 	cli, err := client.NewEnvClient()
--- utils/index.go
+++ utils/index.go
@@ -3,9 +3,7 @@ package utils
 import (
 	"crypto/rand"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 )
 
 func GenerateUuid() string {
@@ -18,24 +16,4 @@ func GenerateUuid() string {
 		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 	fmt.Println(uuid)
 	return uuid
-}
-
-func WriteDockerComposeFile(uuid string, data string) (string, error) {
-
-	_, erro := os.Stat("./composefiles/" + uuid)
-
-	if os.IsNotExist(erro) {
-		errDir := os.MkdirAll("./composefiles/"+uuid, 0755)
-		if errDir != nil {
-			log.Fatal(erro)
-		}
-	}
-	if err := ioutil.WriteFile("./composefiles/"+uuid+"/docker-compose.yml", []byte(data), 0644); err == nil {
-		fmt.Println("Wrote in docker-compose.yml")
-		return uuid, nil
-	} else {
-		fmt.Println(err)
-		return "", err
-	}
-
 }
