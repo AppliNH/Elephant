cf7e4c3f07e1b123f8dc50bda6c39149fb989781
AppliNH <thomas.martindev@gmail.com>
2020-07-18 00:12:34 +0200

    changed db dir bc was creating in the current pth

--- kvdb/init.go
+++ kvdb/init.go
@@ -2,15 +2,22 @@ package kvdb
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/boltdb/bolt"
 )
 
 func InitDB() (*bolt.DB, error) {
-	if _, err := os.Stat("db"); os.IsNotExist(err) {
-		os.Mkdir("db", 0700)
+
+	ex, _ := os.Executable()
+
+	exPath := filepath.Dir(ex)
+
+	if _, err := os.Stat(exPath + "/db"); os.IsNotExist(err) {
+		os.Mkdir(exPath+"/db", 0700)
 	}
-	db, _ := bolt.Open("db/elephant.db", 0644, nil)
+
+	db, _ := bolt.Open(exPath+"/db/elephant.db", 0644, nil)
 
 	err := db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists([]byte("stacks"))
